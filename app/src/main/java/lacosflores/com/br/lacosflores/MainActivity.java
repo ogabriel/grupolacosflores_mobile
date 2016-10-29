2f289ea47dfecf009c7a9303907a673d11aac34f
luizpaulino <lp.luizpaulino@gmail.com>
2016-10-29 18:16:00 -0200

    atualização MainActivity

--- app/src/main/java/lacosflores/com/br/lacosflores/MainActivity.java
+++ app/src/main/java/lacosflores/com/br/lacosflores/MainActivity.java
@@ -13,15 +13,30 @@ import android.widget.Toast;
 import static lacosflores.com.br.lacosflores.R.menu.activity_home_drawer;
 import static lacosflores.com.br.lacosflores.R.menu.home_pedido;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     Button btnLog = (Button)findViewById(R.id.btnLog);
+    EditText edNum = (EditText)findViewById(R.id.edNumero);
+    EditText edCod = (EditText)findViewById(R.id.edCodigo);
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edCod.equals("01") && edNum.equals("12345")){
+                    Intent intent = new Intent(MainActivity.this, PedidoActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "Código/Número inválido", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
 
 }
