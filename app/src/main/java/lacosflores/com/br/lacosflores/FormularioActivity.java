7f480fdc66fb3ad2c1033a18639c4624ae1a643d
luizpaulino <lp.luizpaulino@gmail.com>
2016-10-29 18:13:32 -0200

    a classe FormularioActivity foi refeita

---
+++ app/src/main/java/lacosflores/com/br/lacosflores/FormularioActivity.java
@@ -0,0 +1,15 @@
+package lacosflores.com.br.lacosflores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
