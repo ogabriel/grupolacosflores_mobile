6e1cc571ca51e07cd45109ec81f55789c1275458
luizpaulino <lp.luizpaulino@gmail.com>
2016-11-19 17:57:31 -0200

    Luiz

---
+++ app/src/main/java/lacosflores/com/br/lacosflores/java/dao/ApiaryDAO.java
@@ -0,0 +1,8 @@
+package lacosflores.com.br.lacosflores.java.dao;
+
+/**
+ * Created by SENAI on 19/11/2016.
+ */
+
+public class ApiaryDAO {
+}
---
+++ app/src/main/java/lacosflores/com/br/lacosflores/java/model/Choices.java
@@ -0,0 +1,8 @@
+package lacosflores.com.br.lacosflores.java.model;
+
+/**
+ * Created by SENAI on 19/11/2016.
+ */
+
+public class Choices {
+}
